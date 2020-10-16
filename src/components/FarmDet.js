2c4be087f58573429e3ad4d249a3fbc05621f5d7
Klara Freitag <klara@Klaras-MacBook-Pro.local>
2020-10-16 15:16:50 +0200

    Added dummy photo

--- src/components/FarmDet.js
+++ src/components/FarmDet.js
@@ -5,7 +5,7 @@ function FarmDet() {
 
     return(
         <div className="farm-box">
-            <img className="farm-img" src="" alt=""/>
+            <img className="farm-img" src="https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80" alt=""/>
             <div className="farm-info">
                 <h4 className="farm-title">Fantasy Farm Name</h4>
                 <h6 className="farm-address">Fantasy Street 11, 12345 Fantasy Town</h6>
