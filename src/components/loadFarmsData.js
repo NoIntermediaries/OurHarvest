e13c77c5ebd77a7d667752e87917ef8fb9875313
Konstantinos Megas <nextdoorpanda@gmail.com>
2020-10-20 18:31:30 +0300

    Created auto-generation of farm cards based on data. Added sample data.

---
+++ src/components/loadFarmsData.js
@@ -0,0 +1,57 @@
+export const sampleFarms = [
+  {
+    name: 'Farm One',
+    address: 'Somewhere 23',
+    city: 'Berlin',
+    country: 'Germany',
+    phone: '123456789',
+    email: 'farmone@farmone.site',
+    website: 'farmone.site',
+    products: ['apples', 'potatoes', 'pears'],
+    image: 'https://images.unsplash.com/photo-1534073133331-c4b62a557083?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60'
+  },
+  {
+    name: 'Farm Two',
+    address: 'Somewhere 23',
+    city: 'Berlin',
+    country: 'Germany',
+    phone: '123456789',
+    email: 'farmone@farmone.site',
+    website: 'farmone.site',
+    products: ['apples', 'potatoes', 'pears'],
+    image: 'https://images.unsplash.com/photo-1444858291040-58f756a3bdd6?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'
+  },
+  {
+    name: 'Farm Three',
+    address: 'Somewhere 23',
+    city: 'Berlin',
+    country: 'Germany',
+    phone: '123456789',
+    email: 'farmone@farmone.site',
+    website: 'farmone.site',
+    products: ['apples', 'potatoes', 'pears'],
+    image: 'https://images.unsplash.com/photo-1562046720-654495889645?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60'
+  },
+  {
+    name: 'Farm Four',
+    address: 'Somewhere 23',
+    city: 'Berlin',
+    country: 'Germany',
+    phone: '123456789',
+    email: 'farmone@farmone.site',
+    website: 'farmone.site',
+    products: ['apples', 'potatoes', 'pears'],
+    image: 'https://images.unsplash.com/photo-1486754735734-325b5831c3ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'
+  },
+  {
+    name: 'Farm Five',
+    address: 'Somewhere 23',
+    city: 'Berlin',
+    country: 'Germany',
+    phone: '123456789',
+    email: 'farmone@farmone.site',
+    website: 'farmone.site',
+    products: ['apples', 'potatoes', 'pears'],
+    image: 'https://images.unsplash.com/photo-1585499193151-0f50d54c4e1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60'
+  },
+];
