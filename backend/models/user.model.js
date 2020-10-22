d39786dd8e04fd779aa8a17ccfb64b55b4fa5382
mebe248 <meenuanna.george@dpschool.io>
2020-10-22 20:56:16 +0200

    Adding the database to contact form.

---
+++ backend/models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      //unique: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+     // unique: true,
+      trim: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    // unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    //  unique: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: false,
+    //  unique: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
---
+++ backend/routes/users.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+
+let User = require("../models/user.model");
+
+// To display all users from database
+router.get("/", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => res.status(404).json("Error:" + err));
+});
+
+router.post("/add", (req, res) => {
+  console.log(req.body);
+
+  const username = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phone: req.body.phone,
+    email: req.body.email,
+    message: req.body.message,
+  };
+
+  const newUser = new User(username);
+  console.log(newUser);
+
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.json({
+      user: newUser,
+      body: "User added",
+    });
+  });
+});
+
+module.exports = router;
