428417a6e205ca0c271fbf6be9075f1827fe5d10
Klara Freitag <klara@Klaras-MacBook-Pro.local>
2020-10-30 11:31:13 +0100

    add materialize to FarmDetail

--- src/components/FarmDetail.js
+++ src/components/FarmDetail.js
@@ -22,24 +22,34 @@ class FarmDetail extends Component {
       if (farms.id === this.state.id) {
         return (
           <div className="farm-box" key={farms.id}>
-            <img className="farm-img" src={farms.image} alt="hello" />
-            <div className="farm-info">
-              <h4 className="farm-title">{farms.name}</h4>
-              <h6 className="farm-address">{farms.address}</h6>
-              <h6 className="farm-products">{productSpaced}</h6>
-              <p className="farm-descr">
-                This is a general description of the farm. We are a family-led
-                farm south of Fantasy Town. Feeling fed up with the busy city
-                life? Come visit us and learn about our organically grown veggies
-                and fruits. You will never want to buy anything at a supermarket
-                ever again! If you have any questions in advance, feel free to
-                contact us via phone or email. See you soon!
-            </p>
-              <p className="farm-contact">
-                {farms.phone}
-                {farms.email}
-              </p>
+          <div class="row">
+          <div class="col s12 m7">
+            <div class="card">
+              <div class="card-image detail-img">
+                <img src={farms.image} alt="hello" />
+                <span class="card-title">{farms.name}</span>
+                </div>
+              <div class="card-content">
+                <div className="farm-info">
+                    <h6 className="farm-address">{farms.address}</h6>
+                    <h6 className="farm-products">{productSpaced}</h6>
+                    <p className="farm-descr">
+                        This is a general description of the farm. We are a family-led
+                        farm south of Fantasy Town. Feeling fed up with the busy city
+                        life? Come visit us and learn about our organically grown veggies
+                        and fruits. You will never want to buy anything at a supermarket
+                        ever again! If you have any questions in advance, feel free to
+                        contact us via phone or email. See you soon!
+                    </p>
+                    <p className="farm-contact">
+                        {farms.phone}
+                        {farms.email}
+                    </p>
+                </div>
+                </div>
             </div>
+          </div>
+          </div>
           </div>
         );
       }
